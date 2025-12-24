01e86c914e77462b6168d3ff596ffebf4d53798a
Roshan Ambilwade <ambilwaderoshan@gmail.com>
2025-12-24 22:07:35 +0530

    fix curruser

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
-  res.locals.curruser = req.user;
+  res.locals.curruser = req.user || null;
   next();
 });
 
